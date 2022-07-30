f9fb36c7b3ed545ffe69869af75c5f53672f6378
Ala9921160003 <ala.arman.9921160003@gmail.com>
2022-07-30 12:44:54 +0430

    simple response to POST method has been created in review function

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
 def review(req):
+    if req.methode == "POST":
+        entered_username = req.POST["username"]
+        print(entered_username)
+        return HttpResponseRedirect("/thank_you")
+        
     return render(req, "reviews/review.html")
+
+
+def thank_you(req):
+    return render(req, "reviews/thank_you.html")
