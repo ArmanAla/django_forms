dc9b4af78d50d173c3069e61c6eb8809a40de884
Ala9921160003 <ala.arman.9921160003@gmail.com>
2022-08-01 09:23:20 +0430

    now we check usename is an empty string or not.

--- reviews/views.py
+++ reviews/views.py
@@ -6,10 +6,17 @@ from django.http import HttpResponseRedirect
 def review(req):
     if req.method == "POST":
         entered_username = req.POST["username"]
+        
+        if entered_username == "":
+            return render(req, "reviews/review.html", {
+                "has_error": True
+            })
         print(entered_username)
         return HttpResponseRedirect("/thank_you")
         
-    return render(req, "reviews/review.html")
+    return render(req, "reviews/review.html", {
+        "has__error": False
+    })
 
 
 def thank_you(req):
